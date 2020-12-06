79f3e4b0e271ef3cbe423010d9bf527650741818
iisatorres <65899100+iisatorres@users.noreply.github.com>
2020-12-06 18:09:29 -0300

    Update wsgi.py

--- projeto_dogland/wsgi.py
+++ projeto_dogland/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sos_pet project.
+WSGI config for projeto_dogland project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sos_pet.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_dogland.settings')
 
 application = get_wsgi_application()
